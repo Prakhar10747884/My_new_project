872f1a8c2491b62b2dc4fedc048cec6568d4b43b
Prakhar10747884 <prakhar.10747884@LTIMindtree.com>
2024-12-03 15:57:19 +0530

    Delete US_server directory

--- US_server/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my_backend_bucket"
-    key = "C:/Users/10747884/Desktop/Terraform/terraform.tfstate"
-    region = "us-east-2"
-    encrypt = "true"
-    dynamodb_table = "backend_table"
-}
-}
--- US_server/main.tf
+++
@@ -1,6 +0,0 @@
-module "US_server" {
-source = "C:/Users/10747884/Desktop/Terraform/main.tf"
-ami = "ami-0c55b159cbfaf31f0"
-name = "us_app_server"
-type = "t2.medium"
-}
